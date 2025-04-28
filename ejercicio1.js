726ca2b987dcdf083d3c030672272c9d6c48eefd
Sabrina <20223567@aloe.ulima.edu.pe>
2025-04-28 18:09:24 -0500

    Lógica .js implementada completamente

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,31 +1,37 @@
-let ButEnviarOnClick = function(){
+let butEnviarOnClick = function() {
     let inputNombre = document.getElementById("nombre")
     let inputCorreo = document.getElementById("correo")
     let textAreaMensaje = document.getElementById("mensaje")
-    //1. Verificar si todas as cajas de texto tienen valores
+    let divAreaMensajes = document.getElementById("area_mensajes")
+
+    //1. Verificar si todas las cajas de texto 
+    // tienen valores
     if (inputNombre.value == "" ||
         inputCorreo.value == "" ||
-        textAreaMensaje == "" 
-    ){
-        console.error("Debe ingresar todos los campos")
+        textAreaMensaje.value ==""
+    ) {
+        console.error("Debe ingresar todos los campos.")
+        divAreaMensajes.innerText = "Debe ingresar todos los campos."
         return
+    }
 
-        }
     //2. Verificar el formato el correo (@ y .)
-        if(!inputCorreo.value.includes("@") ||
-           !inputCorreo.value.includes(".")
-        ) {
-            console.error("Error en el formato del mail.")
-            return
-        }
+    if (!inputCorreo.value.includes("@") ||
+        !inputCorreo.value.includes(".")
+    ) {
+        console.error("Error en el formato de email.")
+        divAreaMensajes.innerText = "Error en el formato de email."
+        return
+    }
 
     //3. Pintar que se envio correctamente
-    console.log("Se envió correctamente. ")
+    console.log("Se envio correctamente.")
+    divAreaMensajes.innerText = "Se envio correctamente."
 }
 
-let main = function(){
+let main = function () {
     let butEnviar = document.getElementById("but_enviar")
-    butEnviar.addEventListener("click", ButEnviarOnClick)
+    butEnviar.addEventListener("click", butEnviarOnClick)
 }
 
 main()
